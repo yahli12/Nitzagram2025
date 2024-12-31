aa5637339bc820fbe38cdf841099051f7e5771fc
Nitzanim Admin <72763420+NitzanimAdmin@users.noreply.github.com>
2024-12-31 18:19:08 +0200

    added initial files

---
+++ classes/Post.py
@@ -0,0 +1,53 @@
+import pygame
+
+from constants import *
+from helpers import screen
+
+
+class Post:
+    """
+    A class used to represent post on Nitzagram
+    """
+    def __init__(self): #TODO: add parameters
+        #TODO: write me!
+        pass
+
+    def display(self):
+        """
+        Display the Post image/Text, description, location, likes and comments
+        on screen
+
+        :return: None
+        """
+        # TODO: write me!
+        pass
+
+
+    def display_comments(self):
+        """
+        Display comments on post. In case there are more than 4
+        comments, show only 4 comments chosen by reset_comments_display_index
+
+        :return: None
+        """
+        position_index = self.comments_display_index
+        # If there are more than 4 comments, print "view more comments"
+        if len(self.comments) > NUM_OF_COMMENTS_TO_DISPLAY:
+            comment_font = pygame.font.SysFont('chalkduster.ttf',
+                                               COMMENT_TEXT_SIZE)
+            view_more_comments_button = comment_font.render("view more comments",
+                                                            True, LIGHT_GRAY)
+            screen.blit(view_more_comments_button, (VIEW_MORE_COMMENTS_X_POS,
+                                                    VIEW_MORE_COMMENTS_Y_POS))
+
+        # Display 4 comments starting from comments_display_index
+        for i in range(0, len(self.comments)):
+            if position_index >= len(self.comments):
+                position_index = 0
+            self.comments[position_index].display(i)
+            position_index += 1
+            if i >= NUM_OF_COMMENTS_TO_DISPLAY - 1:
+                break
+
+
+
---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+from helpers import screen
+from constants import WINDOW_WIDTH, WINDOW_HEIGHT, BLACK
+
+
+def main():
+    # Set up the game display, clock and headline
+    pygame.init()
+
+    # Change the title of the window
+    pygame.display.set_caption('Nitzagram')
+
+    clock = pygame.time.Clock()
+
+    # Set up background image
+    background = pygame.image.load('Images/background.png')
+    background = pygame.transform.scale(background,
+                                        (WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    # TODO: add a post here
+
+    running = True
+    while running:
+        # Grabs events such as key pressed, mouse pressed and so.
+        # Going through all the events that happened in the last clock tick
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Display the background, presented Image, likes, comments, tags and location(on the Image)
+        screen.fill(BLACK)
+        screen.blit(background, (0, 0))
+
+        # Update display - without input update everything
+        pygame.display.update()
+
+        # Set the clock tick to be 60 times per second. 60 frames for second.
+        clock.tick(60)
+    pygame.quit()
+    quit()
+
+
+main()
